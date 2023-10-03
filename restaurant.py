3e2731bc8230c20980589297cd89cd976edca154
LudriBazile <71726106+LudriBazile@users.noreply.github.com>
2023-10-02 22:24:48 -0400

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -1,42 +1,53 @@
-
 #CS175
 #Ludrianna Bazile
-#restaurant
+#restaurant with while loop
+
 vegetarian = False
 vegan = False
 gluten_free = False
+continueCode = True
 
-vegetarianResponse = input("Is anyone in your party a vegetarian ")
-if vegetarianResponse == "yes":
-    vegetarian = True
-if vegetarianResponse == "no" :
-    vegetarian == False
+while(continueCode == True):
+    vegetarianResponse = input("Is anyone in your party a vegetarian: ")
+    if vegetarianResponse == "yes":
+        vegetarian = True
+    if vegetarianResponse == "no" :
+        vegetarian == False
 
-veganResponse = input("Is anyone in your party a vegan ")
-if veganResponse == "yes":
-    vegan = True
-if veganResponse == "no" :
-    vegan == False
+    veganResponse = input("Is anyone in your party a vegan: ")
+    if veganResponse == "yes":
+        vegan = True
+    if veganResponse == "no" :
+        vegan == False
 
-gluten_freeResponse = input("Is anyone in your party a gluten free ")
-if gluten_freeResponse == "yes":
-    gluten_free = True
-if gluten_freeResponse == "no" :
-    gluten_free == False
+    gluten_freeResponse = input("Is anyone in your party a gluten free: ")
+    if gluten_freeResponse == "yes":
+        gluten_free = True
+    if gluten_freeResponse == "no" :
+        gluten_free == False
 
-print("\n")
+    print("\n")
 
-print("Here are your restaurant choices.")
+    print("Here are your restaurant choices.")
 
 
-if vegetarian == False and vegan == False and gluten_free == False:
-   print("Joe's Gourmet Burgers")
+    if vegetarian == False and vegan == False and gluten_free == False:
+       print("Joe's Gourmet Burgers")
 
-if vegan == False and gluten_free == False:
-   print("Mama Fine Italian")
+    if vegan == False and gluten_free == False:
+       print("Mama Fine Italian")
 
-if vegan == False:
-   print("Main Street Pizza")
+    if vegan == False:
+       print("Main Street Pizza")
 
-print("Corner Café")
-print("The Chef's Kitchen") 
+    print("Corner Café")
+    print("The Chef's Kitchen")
+    print("\n")
+    
+    continueResponse = input("Enter 'yes' if you would like to do another restaurant search (enter 'no' to end): ")
+    
+    if continueResponse == "yes":
+        continueCode = True
+    if continueResponse == "no" :
+        break
+    print("\n")

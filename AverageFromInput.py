53211c01ef14d82909203ae2eb29751bf031bfab
LudriBazile <71726106+LudriBazile@users.noreply.github.com>
2023-12-06 12:00:15 -0500

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -3,26 +3,45 @@
 #AverageFromInput
 
 
-def main():
-    total = 0
-    number = 0
-    counter = 1
-    average = 0
 
+def getValues():
+    
+    
     f = open('numbers.txt','r')
+    number = 0
 
+    global total
+    total = 0
+
+    global counter 
+    counter = 1
+    
     for i in f:
         number = int(i)
         total = total + number
-        print ("I read in ",counter,"number(s) Current number is:    ",number," Total is:    ", total)
+        print (f"I read in {counter} number(s) Current number is: {number} Total is: {total}")
         counter = counter + 1
     f.close()
+    
 
-    average = total / (counter-1)
+    
+def getAverage():
+    global average
+    average = (total/(counter-1))
+    
+    
+def printAverage():
+    print(f"Average = {average}")
 
-    print("Average: ",average)
+def main():
+    
+    getValues()
+    getAverage()
+    printAverage()
+
+    
 
 
 if __name__ == '__main__':
     main()
-    
+
